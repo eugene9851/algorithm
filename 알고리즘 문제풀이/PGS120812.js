30ee2c85b9fcef909b301068064a756b6ea9eac6
eugene9851 <eugene9851@naver.com>
2024-02-08 16:12:36 +0900

    최빈값 리팩토링

--- 알고리즘 문제풀이/PGS120812.js
+++ 알고리즘 문제풀이/PGS120812.js
@@ -8,23 +8,43 @@
 
 array = [1, 1, 2, 2, 2]
 
+// function solution(array) {
+//   let obj = {}
+//   for(let i=0; i<array.length; i++) {
+//     obj[array[i]] ? obj[array[i]]++ : obj[array[i]]=1
+//   }
+//   let arr = []
+//   let keys = []
+//   for(let key in obj) {
+//     keys.push(key)
+//     arr.push(obj[key])
+//   }
+//   let n = Math.max(...arr)
+//   let cnt = 0
+//   for(let i=0; i<arr.length; i++) {
+//     if(arr[i]===n) cnt++
+//     if(cnt==2) break
+//   }
+//   return cnt===1 ? Number(keys[arr.indexOf(n)]) : -1
+// }
+
+//리팩토링1: Object.values(obj) => obj의 value들만 뽑아 array로 반환
+//리팩토링2: Object.keys(obj) => obj의 key들만 뽑아 array로 반환
+//리팩토링3: for문 안에 cnt++ 대신 filter.length
+//리팩토링4: reduce함수를 통해 map구현
+
 function solution(array) {
-  let obj = {}
-  for(let i=0; i<array.length; i++) {
-    obj[array[i]] ? obj[array[i]]++ : obj[array[i]]=1
-  }
-  let arr = []
-  let keys = []
-  for(let key in obj) {
-    keys.push(key)
-    arr.push(obj[key])
-  }
-  let n = Math.max(...arr)
-  let cnt = 0
-  for(let i=0; i<arr.length; i++) {
-    if(arr[i]===n) cnt++
-    if(cnt==2) break
-  }
-  return cnt===1 ? Number(keys[arr.indexOf(n)]) : -1
+  const frequencyMap = array.reduce((acc, cur) => {
+    acc[cur] = (acc[cur] || 0) + 1
+    return acc
+  }, {})
+
+  const frequencyValues = Object.values(frequencyMap)
+  const frequencyKeys = Object.keys(frequencyMap)
+  const maxValue = Math.max(...frequencyValues)
+
+  const maxValueCount = frequencyValues.filter((frequencyValue) => frequencyValue===maxValue).length
+
+  return maxValueCount===1 ? Number(frequencyKeys[frequencyValues.indexOf(maxValue)]) : -1
 }
 console.log(solution(array))
