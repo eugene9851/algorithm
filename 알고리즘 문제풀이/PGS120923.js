5d0e6311cfcbe65f64ccc08bb6742bec65f32283
eugene9851 <eugene9851@naver.com>
2024-02-13 15:30:27 +0900

    연속된 수의 합 리팩토링

--- 알고리즘 문제풀이/PGS120923.js
+++ 알고리즘 문제풀이/PGS120923.js
@@ -1,12 +1,23 @@
 function solution(num, total) {
-  let sum = total%2===1 ? num*(num-1)/2 : (num-1)*(num-2)/2 + (num-1)
+  let sum =
+    total % 2 === 1
+      ? (num * (num - 1)) / 2
+      : ((num - 1) * (num - 2)) / 2 + (num - 1);
 
-  const start = (total-sum)/num
-  let answer = []
-  for(let i=0; i<num; i++) {
-    answer.push(start+i)
+  const start = (total - sum) / num;
+  let answer = [];
+  for (let i = 0; i < num; i++) {
+    answer.push(start + i);
   }
-  return answer
+  return answer;
 }
 
-console.log(solution(5,5))
+//리팩토링1 : Array.from({length:_}, (value, index) => i) => 길이가 _인 배열을 리턴
+
+function solution(num, total) {
+  const avg = total / num;
+  const start = avg - (num - 1) / 2;
+
+  return Array.from({ length: num }, (_, i) => start + i);
+}
+console.log(solution(5, 5));
