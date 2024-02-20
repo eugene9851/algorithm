098c6db4cf402655a0ae72a2bc1ee514a8ed9bd3
eugene9851 <eugene9851@naver.com>
2024-02-20 11:47:38 +0900

    프로그래머스 K번째수

---
+++ 알고리즘 문제풀이/PGS42748.js
@@ -0,0 +1,14 @@
+function solution(array, commands) {
+  return commands.map(([i, j, k]) => {
+    return array.slice(i - 1, j).sort((a, b) => a - b)[k - 1];
+  });
+}
+
+array = [1, 5, 2, 6, 3, 7, 4];
+commands = [
+  [2, 5, 3],
+  [4, 4, 1],
+  [1, 7, 3],
+];
+
+console.log(solution(array, commands));
