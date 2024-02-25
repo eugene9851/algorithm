e3623d307bcaec013677e417c920383f25ad197f
eugene9851 <eugenelee980713@gmail.com>
2024-02-25 16:47:10 +0900

    예산 리팩토링

--- 알고리즘 문제풀이/PGS12982.js
+++ 알고리즘 문제풀이/PGS12982.js
@@ -2,14 +2,25 @@ function solution(d, budget) {
   const sortedArray = d.sort((a, b) => a - b);
   let totalCost = 0;
 
-  for (let i = 0; i < sortedArray.length; i++) {
-    if (totalCost + sortedArray[i] <= budget) {
-      totalCost += sortedArray[i];
-    } else {
-      return i;
-    }
+  // for (let i = 0; i < sortedArray.length; i++) {
+  //   if (totalCost + sortedArray[i] <= budget) {
+  //     totalCost += sortedArray[i];
+  //   } else {
+  //     return i;
+  //   }
+  // }
+  // return sortedArray.length;
+
+  //리팩토링1: for of문을 통해 early return
+
+  let count = 0;
+
+  for (const cost of sortedArray) {
+    totalCost += cost;
+    if (totalCost > budget) break;
+    count++;
   }
-  return sortedArray.length;
+  return count;
 }
 
 d = [1, 3, 2, 5, 4];
