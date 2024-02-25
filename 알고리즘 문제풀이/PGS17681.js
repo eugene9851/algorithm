e678f3717f1f2f3599964baa4a2f90ab0e0cf83a
eugene9851 <eugenelee980713@gmail.com>
2024-02-25 17:09:42 +0900

    [1차] 비밀지도 리팩토링

--- 알고리즘 문제풀이/PGS17681.js
+++ 알고리즘 문제풀이/PGS17681.js
@@ -1,32 +1,38 @@
-function mapArray(n, array) {
-  return array.map((x) => {
-    if (x.toString(2).length < n) {
-      return "0".repeat(n - x.toString(2).length) + x.toString(2);
-    } else {
-      return x.toString(2);
-    }
-  });
-}
+// function mapArray(n, array) {
+//   return array.map((x) => {
+//     if (x.toString(2).length < n) {
+//       return "0".repeat(n - x.toString(2).length) + x.toString(2);
+//     } else {
+//       return x.toString(2);
+//     }
+//   });
+// }
+
+//리팩토링1: padStart함수를 사용하여 함수 간결하게 표현하기
+//         padStart(자릿수, 채울 string)
+//리팩토링2: 정규식과 replace메서드 사용
 
 function solution(n, arr1, arr2) {
-  const mapArr1 = mapArray(n, arr1);
-  const mapArr2 = mapArray(n, arr2);
+  return arr1.map((val, idx) => {
+    //두 배열 중 하나라도 1인 경우 판별
+    const mapRow = (val | arr2[idx]).toString(2).padStart(n, "0");
+    return mapRow.replace(/1|0/g, (m) => (m === "1" ? "#" : " "));
+  });
+  // let answerMap = new Array(n).fill(null).map(() => new Array(n).fill(0));
 
-  let answerMap = new Array(n).fill(null).map(() => new Array(n).fill(0));
+  // for (let i = 0; i < n; i++) {
+  //   for (let j = 0; j < n; j++) {
+  //     if (mapArr1[i][j] === "0" && mapArr2[i][j] === "0") {
+  //       answerMap[i][j] = " ";
+  //     } else {
+  //       answerMap[i][j] = "#";
+  //     }
+  //   }
+  // }
 
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (mapArr1[i][j] === "0" && mapArr2[i][j] === "0") {
-        answerMap[i][j] = " ";
-      } else {
-        answerMap[i][j] = "#";
-      }
-    }
-  }
-
-  for (let i = 0; i < n; i++) {
-    return answerMap.map((row) => row.join(""));
-  }
+  // for (let i = 0; i < n; i++) {
+  //   return answerMap.map((row) => row.join(""));
+  // }
 }
 
 n = 5;
