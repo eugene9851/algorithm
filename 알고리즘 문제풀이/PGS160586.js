8c39c42fb90a1c1a84c5aaf749f8425906dad54a
eugene9851 <eugenelee980713@gmail.com>
2024-02-25 17:16:18 +0900

    대충 만든 자판 리팩토링

--- 알고리즘 문제풀이/PGS160586.js
+++ 알고리즘 문제풀이/PGS160586.js
@@ -12,7 +12,10 @@ function solution(keymap, targets) {
   return targets.map((target) => {
     let count = 0;
     for (let i = 0; i < target.length; i++) {
-      count += findAlphabet(keymap, target[i]);
+      const result = findAlphabet(keymap, target[i]);
+      //알파벳이 키맵에 없으면 즉시 -1 반환
+      if (result === -1) return -1;
+      count += result;
     }
     return count;
   });
