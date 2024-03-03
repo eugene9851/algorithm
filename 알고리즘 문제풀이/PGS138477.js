5cbd587df81eb195016e9cd1c2f3f306fa157f1d
eugene9851 <eugenelee980713@gmail.com>
2024-03-03 14:49:43 +0900

    프로그래머스 명예의 전당 리팩토링

--- 알고리즘 문제풀이/PGS138477.js
+++ 알고리즘 문제풀이/PGS138477.js
@@ -1,19 +1,19 @@
-//명예의 전당 top K
-function topK(k, score, honorArea) {
-  //k일차까지는 명예의 전당에 무조건 입성
-  if (honorArea.length < k) {
-    honorArea.push(score);
-  } else if (honorArea[k - 1] < score) {
-    //명예의 전당에 k개의 점수가 꽉 차있고,
-    //새로 발표된 점수가 명예의 전당의 가장 작은 점수보다 크면
-    //가장 작은 점수 빼고 새로운 점수 넣기
-    honorArea.pop();
-    honorArea.push(score);
-  }
-  //내림차순 정렬(마지막 점수를 정답 배열에 넣기 위해)
-  sortedHonorArea = honorArea.sort((a, b) => b - a);
-  return sortedHonorArea;
-}
+// //명예의 전당 top K
+// function topK(k, score, honorArea) {
+//   //k일차까지는 명예의 전당에 무조건 입성
+//   if(honorArea.length < k) {
+//     honorArea.push(score);
+//   } else if(honorArea[k - 1] < score) {
+//     //명예의 전당에 k개의 점수가 꽉 차있고,
+//     //새로 발표된 점수가 명예의 전당의 가장 작은 점수보다 크면
+//     //가장 작은 점수 빼고 새로운 점수 넣기
+//     honorArea.pop();
+//     honorArea.push(score);
+//   }
+//   //내림차순 정렬(마지막 점수를 정답 배열에 넣기 위해)
+//   sortedHonorArea = honorArea.sort((a, b) => b - a);
+//   return sortedHonorArea;
+// }
 
 function solution(k, scores) {
   //명예의 전당
@@ -25,10 +25,21 @@ function solution(k, scores) {
   //발표점수의 초기값은 무조건 1일차 점수
   let prevScore = scores[0];
 
-  for (let i = 0; i < scores.length; i++) {
-    honorArea = topK(k, scores[i], honorArea);
-    prevScore = honorArea[honorArea.length - 1];
-    answer.push(prevScore);
+  for(let i = 0; i < scores.length; i++) {
+    // honorArea = topK(k, scores[i], honorArea);
+    // prevScore = honorArea[honorArea.length - 1];
+    // answer.push(prevScore);
+
+    if(i < k) {
+      honorArea.push(scores[i])
+      honorArea.sort((a, b) => a - b) //오름차순 정렬
+    } else if(scores[i] > honorArea[0]) {
+      honorArea.shift()
+      honorArea.push(scores[i])
+      honorArea.sort((a, b) => a - b)
+    }
+
+    answer.push(honorArea[0])
   }
   return answer;
 }
