bc293e301013fabe37c61b108d5437d8b0f62af9
eugene9851 <eugenelee980713@gmail.com>
2024-03-03 15:03:35 +0900

    프로그래머스 로또의 최고 순위와 최저 순위 리팩토링

--- 알고리즘 문제풀이/PGS77484.js
+++ 알고리즘 문제풀이/PGS77484.js
@@ -8,23 +8,28 @@ const lottoGrade = {
   4: 3,
   3: 4,
   2: 5,
-  1: 6,
-  0: 6
 }
 
 function solution(lottos, winNums) {
   //당첨 수의 개수
-  let count = 0
+  // let count = 0
 
-  lottos.forEach((lottoNum) => {
-    winNums.includes(lottoNum) && count++
-  })
+  // lottos.forEach((lottoNum) => {
+  //   winNums.includes(lottoNum) && count++
+  // })
+
+  const count = lottos.filter((lotto) => winNums.includes(lotto) && lotto !== 0).length
 
   //0의 개수
   const zeroCount = lottos.filter((lotto) => lotto === 0).length
 
   const maxCount = count + zeroCount
-  return [lottoGrade[maxCount], lottoGrade[count]]
+
+  //등수 결정: 일치하는 번호의 개수가 등수 테이블에 없는 경우, 6등 반환
+  const maxRank = lottoGrade[maxCount] || 6
+  const minRank = lottoGrade[count] || 6
+
+  return [maxRank, minRank]
 }
 
 lottos = [44, 1, 0, 0, 31, 25]
