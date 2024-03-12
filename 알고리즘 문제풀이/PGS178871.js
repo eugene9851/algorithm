494e6c3d81bdeb7b7d1af4769eebee2df4d22e6b
eugene9851 <eugenelee980713@gmail.com>
2024-03-13 00:27:58 +0900

    프로그래머스 달리기 경주 리팩토링

--- 알고리즘 문제풀이/PGS178871.js
+++ 알고리즘 문제풀이/PGS178871.js
@@ -1,39 +1,30 @@
-//이름이 불리면 players 순서 바꾸는 함수
-// function changePlayers(name, players) {
-//   const playerIndex = players.indexOf(name)
-//   let temp = players[playerIndex - 1]
-//   players[playerIndex - 1] = players[playerIndex]
-//   players[playerIndex] = temp
-// }
-
-
 function solution(players, callings) {
-  callings.forEach((name) => {
-    //구조분해할당을 이용한 순서 변경 : error
-    const playerIndex = players.indexOf(name)
-    // let prevPlayer = players[playerIndex - 1]
-    // let latePlayer = players[playerIndex]
-    // [prevPlayer, latePlayer] = [latePlayer, prevPlayer]
+  //플레이어의 이름과 현재 인덱스를 매핑하는 객체 초기화
+  //객체를 이용하여 시간초과 문제 해결
+  let playerIndices = {}
+  players.forEach((player, index) => {
+    playerIndices[player] = index
+  })
 
-    //순서 변경 : 시간 초과
-    let temp = players[playerIndex - 1]
-    players[playerIndex - 1] = players[playerIndex]
-    players[playerIndex] = temp
+  callings.forEach((name) => {
+    const index = playerIndices[name]
+    if(index > 0) { //첫 번째 위치의 플레이어가 아니면
+      const prevName = players[index - 1]
+      players[index - 1] = name
+      players[index] = prevName
+
+      //플레이어 인덱스 매핑 업데이트
+      playerIndices[name] = index - 1
+      playerIndices[prevName] = index
+    }
   })
   return players
 }
 
-//object에 이름 불린 횟수 넣고 풀기 시도
-// function solution(players, callings) {
-//   let callingCountObj = {}
-//   callings.forEach((name) => {
-//     callingCountObj[name] = callingCountObj[name] ? callingCountObj[name] + 1 : 1
-//   })
-//   console.log(callingCountObj)
-//   players.forEach((player, index) => {
-//     index - callingCountObj[player]
-//   })
-// }
+players = ["mumu", "soe", "poe", "kai", "mine"]
+callings = ["kai", "kai", "mine", "mine"]
+
+console.log(solution(players, callings))
 
 players = ["mumu", "soe", "poe", "kai", "mine"]
 callings = ["kai", "kai", "mine", "mine"]
