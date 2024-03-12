4c1abd245cca8b8b2e1e289474c693e8c977e19a
eugene9851 <eugenelee980713@gmail.com>
2024-03-13 00:27:11 +0900

    프로그래머스 붕대 감기 리팩토링

--- 알고리즘 문제풀이/PGS250137.js
+++ 알고리즘 문제풀이/PGS250137.js
@@ -1,12 +1,13 @@
-//11.13~  +20
 //health가 최대치일 때 최대치, 최대치가 아닐 때 그 값을 리턴하는 함수
 function getHealth(health, initialHealth) {
-  if(health >= initialHealth) health = initialHealth
-  else health
-  return health
+  return Math.min(health, initialHealth)
 }
 
 function solution(bandage, initialHealth, attacks) {
+  const BANDAGE_CAST_TIME = bandage[0];
+  const BANDAGE_RECOVERY_RATE = bandage[1];
+  const BANDAGE_ADDITIONAL_RECOVERY = bandage[2];
+
   const lastAttack = attacks[attacks.length - 1][0] //마지막 공격 시간
   let attackIndex = 0 //attacks의 index 0부터 돌기
   let recoveryCount = 0 //연속 성공 횟수
@@ -17,7 +18,7 @@ function solution(bandage, initialHealth, attacks) {
   //time이 마지막 공격 시간 이하이고 health가 0 초과일 때 유효
   while(time <= lastAttack && health > 0) {
     //time이 공격 시간과 일치하면 체력 감소, attackIndex 1 증가, 연속성공 초기화
-    if(time === attacks[attackIndex][0]) {
+    if(attackIndex < attacks.length && time === attacks[attackIndex][0]) {
       health -= attacks[attackIndex][1]
       attackIndex++
       recoveryCount = 0
@@ -25,12 +26,12 @@ function solution(bandage, initialHealth, attacks) {
       //time이 공격 시간과 일치하지 않으면 연속 성공 1 증가
       recoveryCount++
       //증가된 연속성공이 시전 시간과 같다면 기존 health에 초당 회복량+추가 회복량 더하기(최대 initialHealth), 연속성공 초기화
-      if(recoveryCount === bandage[0]) {
-        health = getHealth((health + bandage[2] + bandage[1]), initialHealth)
+      if(recoveryCount === BANDAGE_CAST_TIME) {
+        health = getHealth((health + BANDAGE_RECOVERY_RATE + BANDAGE_ADDITIONAL_RECOVERY), initialHealth)
         recoveryCount = 0
       } else {
         //증가된 연속성공이 시전 시간과 같지 않다면 기존 health에 초당 회복량만 더하기(최대 initialHealth)
-        health = getHealth((health + bandage[1]), initialHealth)
+        health = getHealth((health + BANDAGE_RECOVERY_RATE), initialHealth)
       }
     }
     //time 1 증가
