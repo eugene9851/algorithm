cbc15059506ef2d26a4785214baa82eecc56bc68
eugene9851 <eugenelee980713@gmail.com>
2024-03-13 12:20:45 +0900

    바탕화면 정리 리팩토링

--- 알고리즘 문제풀이/PGS161990.js
+++ 알고리즘 문제풀이/PGS161990.js
@@ -1,29 +1,21 @@
 //파일 존재하는 좌표 구하고, 최소 x,y값 = lux, luy & 최대 x+1, y+1값 = rdx, rdy
 
 function solution(wallpaper) {
+  let minX = Infinity, minY = Infinity, maxX = -1, maxY = -1
+
   //파일 존재하는 좌표 배열
-  let paperMap = []
   for(let i = 0; i < wallpaper.length; i++) {
-    for(let j = 0; j < wallpaper[0].length; j++) {
+    for(let j = 0; j < wallpaper[i].length; j++) {
       if(wallpaper[i][j] === "#") {
-        paperMap.push([i, j])
+        minX = Math.min(minX, i)
+        minY = Math.min(minY, j)
+        maxX = Math.max(maxX, i)
+        maxY = Math.max(maxY, j)
       }
     }
   }
 
-  let paperMapOfX = []
-  let paperMapOfY = []
-  paperMap.forEach(([x, y]) => {
-    paperMapOfX.push(x)
-    paperMapOfY.push(y)
-  })
-
-  const lux = Math.min(...paperMapOfX)
-  const luy = Math.min(...paperMapOfY)
-  const rdx = Math.max(...paperMapOfX) + 1
-  const rdy = Math.max(...paperMapOfY) + 1
-
-  return [lux, luy, rdx, rdy]
+  return [minX, minY, maxX + 1, maxY + 1]
 }
 
 wallpaper = ["..........", ".....#....", "......##..", "...##.....", "....#....."]
