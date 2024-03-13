11ba1db73f1b3e6526b1ccb754ef235ce24d3ffd
eugene9851 <eugenelee980713@gmail.com>
2024-03-13 12:40:47 +0900

    신규 아이디 추천 리팩토링

--- 알고리즘 문제풀이/PGS72410.js
+++ 알고리즘 문제풀이/PGS72410.js
@@ -1,54 +1,37 @@
 function countDot(string) {
-  let count = 0
-
-  const dotIndex = string.indexOf('.')
-
-  for(let i = dotIndex; i < string.length; i++) {
-    if(string[i] === '.') count++
-    else break
-  }
-  if(count === 1) return string
-
-  // string = string.split('').splice(dotIndex + 1, count - 1).join('')
-  else {
-    let newString = string.split('').slice(0, dotIndex).join('') + string.split('').slice(dotIndex + count).join('')
-    return countDot(newString)
-  }
+  return string.replace(/\.{2,}/g, ".")
 }
 
 function solution(newID) {
   //1단계 소문자로 치환
   newID = newID.toLowerCase()
 
+
   //2단계 해당하지 않는 문자 제거
   const reg = /[~!@#$%^&*()=+[{\]}:?,<>/]/g
   newID = newID.replace(reg, "")
+
 
   //3단계 . 연속부분 하나로 치환
   newID = countDot(newID)
   console.log(newID)
 
   //4단계 처음과 끝 . 있으면 제거
-  const idArray = newID.split('')
-  if(idArray[0] === ".") idArray.shift()
-  if(idArray[idArray.length - 1] === '.') idArray.pop()
-
-  newID = idArray.join('')
+  if(newID.startsWith(".")) newID = newID.substring(1)
+  if(newID.endsWith(".")) newID = newID.slice(0, -1)
 
   //5단계 빈문자열이면 'a'대입
   if(newID === "") newID = "a"
 
   //6단계 16자 이상이면 첫15개 문자 이외 모두 제거, 마지막이 .이면 제거
   if(newID.length >= 16) {
-    let newArray = newID.split('').slice(0, 15)
-    if(newArray[newArray.length - 1] === '.') newArray.pop()
-
-    newID = newArray.join('')
+    newID = newID.slice(0, 15)
+    if(newID.endsWith(".")) newID = newID.slice(0, -1)
   }
 
   //7단계 2자 이하면 마지막 문자 반복
-  if(newID.length <= 2) {
-    newID += newID[newID.length - 1].repeat(3 - newID.length)
+  while(newID.length <= 2) {
+    newID += newID.charAt(newID.length - 1)
   }
   return newID
 }
