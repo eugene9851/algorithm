35b8bf3e26ad532ea83c390db47dec4da51508ce
eugene9851 <eugenelee980713@gmail.com>
2024-03-13 21:44:03 +0900

    프로그래머스 두 개 뽑아서 더하기

---
+++ 알고리즘 문제풀이/PGS68644.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+  let resultSet = new Set()
+  for(let i = 0; i < numbers.length - 1; i++) {
+    for(let j = i + 1; j < numbers.length; j++) {
+      resultSet.add(numbers[i] + numbers[j])
+    }
+  }
+  return [...resultSet].sort((a, b) => a - b)
+}
+
+numbers = [2, 1, 3, 4, 1]
+
+console.log(solution(numbers))
