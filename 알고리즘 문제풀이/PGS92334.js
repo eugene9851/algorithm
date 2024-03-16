71e0c6d4e48e6f7525aef3ae8993fb8301145cbc
eugene9851 <eugenelee980713@gmail.com>
2024-03-16 22:31:34 +0900

    신고결과 받기 리팩토링

--- 알고리즘 문제풀이/PGS92334.js
+++ 알고리즘 문제풀이/PGS92334.js
@@ -1,41 +1,33 @@
 function solution(idList, report, minCount) {
-  //중복 신고는 1회로 처리, array로 다시 변경
-  const reportSet = new Set(report)
-  const reportArray = Array.from(reportSet)
+  const uniqueReports = new Set(report) //중복 제거된 신고 목록
+  const reportsReceived = {} //각 사용자 별 신고 받은 횟수
+  const reportsMade = {} //각 사용자 별 신고한 대상
 
-  //이름 별 불린 횟수 객체로 모으기
-  let reportedNameCount = {}
-  reportArray.forEach((reportName) => {
-    let reportedName = reportName.split(' ')[1]
-    reportedNameCount[reportedName] ? reportedNameCount[reportedName] = reportedNameCount[reportedName] + 1 : reportedNameCount[reportedName] = 1
-  })
+  uniqueReports.forEach(report => {
+    const [reporter, reported] = report.split(' ')
 
-  //불린 횟수가 minCount회 이상인 이름끼리 배열로 모으기
-  let outNames = []
-  idList.forEach((name) => {
-    reportedNameCount[name] >= minCount && outNames.push(name)
-  })
-
-  //report array에서 정지된 아이디를 신고한 문자열만 남기기
-  const reportOutArray = reportArray.filter((reportName) => {
-    let reportedName = reportName.split(' ')[1]
-    if(outNames.includes(reportedName)) {
-      return reportedName
+    //reportsReceived
+    if(!reportsReceived[reported]) {
+      reportsReceived[reported] = 0
     }
+    reportsReceived[reported]++
+
+    //reportsMade
+    if(!reportsMade[reporter]) {
+      reportsMade[reporter] = new Set()
+    }
+    reportsMade[reporter].add(reported)
   })
 
-  //정지된 아이디를 신고한 유저들의 배열
-  const userOutArray = reportOutArray.map((string) => {
-    return string.split(' ')[0]
+  const results = idList.map(userId => {
+    const reportedUsers = reportsMade[userId] || []
+    console.log(Array.from(reportedUsers))
+    return Array.from(reportedUsers).reduce((count, user) => {
+      return count + (reportsReceived[user] >= minCount ? 1 : 0)
+    }, 0)
   })
 
-  let answer = new Array(idList.length).fill(0)
-  for(let i = 0; i < idList.length; i++) {
-    userOutArray.forEach((userName) => {
-      idList[i] === userName && answer[i]++
-    })
-  }
-  return answer
+  return results
 }
 
 idList = ["muzi", "frodo", "apeach", "neo"]
