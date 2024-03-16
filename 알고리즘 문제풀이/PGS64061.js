b52732184abffa14d9cea3720f8c708ba70b5648
eugene9851 <eugenelee980713@gmail.com>
2024-03-16 23:48:13 +0900

    크레인 인형뽑기 게임 retry

--- 알고리즘 문제풀이/PGS64061.js
+++ 알고리즘 문제풀이/PGS64061.js
@@ -1,39 +1,31 @@
 //resultMap에서 연속으로 중복된 인형 있으면 count+1, 결과적으로 count 리턴
 let count = 0
-function popDolls(resultMap) {
-
-  if(resultMap.length <= 1) return count
-  if(resultMap.length === 2 && resultMap[0] !== resultMap[1]) return count
-
-  let i = 0
-  while(i < resultMap.length) {
-    if(resultMap[i] === resultMap[i + 1]) {
+function popDolls(array) {
+  for(let i = 0; i < array.length - 1; i++) {
+    if(array[i] === array[i + 1]) {
       count++
-      i += 2
-      popDolls(resultMap.splice(i, 2))
-      break
+      array.splice(i, 2)
+      return popDolls(array)
     }
-    else i++
   }
   return count
 }
 
 function solution(board, moves) {
-  //빈칸인 부분 지우기
-  const newBoard = board.map((line) => {
-    return line.filter((doll) => doll !== 0)
+  let stack = []
+  //뽑은 인형 stack에 넣기
+  moves.forEach((move) => {
+    for(let i = 0; i < 5; i++) {
+      if(board[i][move - 1] !== 0) {
+        stack.push(board[i][move - 1])
+        board[i][move - 1] = 0
+        break
+      }
+    }
   })
-
-  //뽑은 인형 moveMap에 넣기
-  let moveMap = moves.map((move) => {
-    return newBoard[move - 1].pop()
-  })
-
-  //뽑은 인형이 없을 때 undefined가 들어갔으므로 undefined 제거
-  const resultMap = moveMap.filter((number) => number !== undefined)
 
   //count 1회 당 두 개의 인형이 사라지므로 2배로 리턴
-  return popDolls(resultMap) * 2
+  return popDolls(stack) * 2
 }
 
 board = [[0, 0, 0, 0, 0], [0, 0, 1, 0, 3], [0, 2, 5, 0, 1], [4, 2, 4, 4, 2], [3, 5, 1, 3, 1]]
