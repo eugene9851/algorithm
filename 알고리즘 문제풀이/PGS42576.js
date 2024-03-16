8287a67723e637a079c4abf78d04e90456138223
eugene9851 <eugenelee980713@gmail.com>
2024-03-16 23:03:07 +0900

    완주하지 못한 선수 리팩토링

--- 알고리즘 문제풀이/PGS42576.js
+++ 알고리즘 문제풀이/PGS42576.js
@@ -1,11 +1,22 @@
 function solution(participants, completion) {
-  //두 배열을 알파벳순으로 정렬
-  participants.sort()
-  completion.sort()
+  const participantCount = new Map()
 
-  //같은 Index에서 요소의 값이 다르면 그 부분이 완주하지 못한 사람
-  for(let i = 0; i < participants.length; i++) {
-    if(participants[i] !== completion[i]) return participants[i]
+  //참가자의 이름과 출현 횟수 매핑
+  participants.forEach((name) => {
+    if(!participantCount.has(name)) participantCount.set(name, 1)
+    else participantCount.set(name, participantCount.get(name) + 1)
+  })
+
+  console.log(participantCount)
+
+  //완주한 선수들의 이름에 따라 카운트 감소
+  completion.forEach((name) => {
+    if(participantCount.has(name)) participantCount.set(name, participantCount.get(name) - 1)
+  })
+
+  //완주하지 못한 선수 찾기
+  for(let [name, count] of participantCount) {
+    if(count > 0) return name
   }
 }
 
