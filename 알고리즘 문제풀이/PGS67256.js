209d912bae9637f431addee2f6ce3d3e894ea499
eugene9851 <eugenelee980713@gmail.com>
2024-03-16 22:56:03 +0900

    키패드 누르기 리팩토링

--- 알고리즘 문제풀이/PGS67256.js
+++ 알고리즘 문제풀이/PGS67256.js
@@ -1,13 +1,10 @@
 const pad = [[1, 2, 3], [4, 5, 6], [7, 8, 9], ["*", 0, "#"]]
 
-//숫자의 좌표 구하기
-function findIndex(number) {
-  for(let i = 0; i < 4; i++) {
-    for(let j = 0; j < 3; j++) {
-      if(pad[i][j] === number) return [i, j]
-    }
-  }
-}
+//숫자의 좌표 미리 계산
+const coordinates = {}
+pad.forEach((row, i) => {
+  row.forEach((num, j) => coordinates[num] = [i, j])
+})
 
 //현재 좌표에서 다음 숫자까지의 거리 구하기
 function findDistance(currentIndex, numberIndex) {
@@ -17,35 +14,37 @@ function findDistance(currentIndex, numberIndex) {
 function solution(numbers, hand) {
   let left = [3, 0] //왼손의 시작 지점
   let right = [3, 2] //오른손의 시작 지점
+  let result = ""
 
-  const result = numbers.map((number) => {
+  numbers.map((number) => {
+    const numIndex = coordinates[number]
+
     //1,4,7이면 왼손
-    if(number === 1 || number === 4 || number === 7) {
-      left = findIndex(number)
-      return 'L'
+    if([1, 4, 7].includes(number)) {
+      left = numIndex
+      result += 'L'
     }
     //3,6,9이면 오른손
-    if(number === 3 || number === 6 || number === 9) {
-      right = findIndex(number)
-      return 'R'
+    if([3, 6, 9].includes(number)) {
+      right = numIndex
+      result += 'R'
     }
     //2,5,8,0이면 가까운 거리에 있는 손
-    if(number === 2 || number === 5 || number === 8 || number === 0) {
-      if(findDistance(right, findIndex(number)) > findDistance(left, findIndex(number))) {
-        left = findIndex(number)
-        return 'L'
-      } else if(findDistance(right, findIndex(number)) < findDistance(left, findIndex(number))) {
-        right = findIndex(number)
-        return 'R'
+    if([2, 5, 8, 0].includes(number)) {
+      const leftDist = findDistance(left, numIndex)
+      const rightDist = findDistance(right, numIndex)
+
+      if(leftDist < rightDist || (rightDist === leftDist && hand === 'left')) {
+        left = numIndex
+        result += 'L'
       } else {
-        //거리가 같으면 왼손잡이는 왼손, 오른손잡이는 오른손
-        hand === 'left' ? left = findIndex(number) : right = findIndex(number)
-        return hand[0].toUpperCase()
+        right = numIndex
+        result += 'R'
       }
     }
   })
-  //배열인 result를 문자열로 변환
-  return result.join('')
+
+  return result
 }
 
 numbers = [7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]
