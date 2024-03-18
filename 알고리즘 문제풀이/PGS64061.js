0695155c42aca3e1ff3d5b8dd25b9f242c27056c
eugene9851 <eugenelee980713@gmail.com>
2024-03-18 17:11:08 +0900

    크레인 인형뽑기 게임 리팩토링

--- 알고리즘 문제풀이/PGS64061.js
+++ 알고리즘 문제풀이/PGS64061.js
@@ -1,31 +1,23 @@
-//resultMap에서 연속으로 중복된 인형 있으면 count+1, 결과적으로 count 리턴
-let count = 0
-function popDolls(array) {
-  for(let i = 0; i < array.length - 1; i++) {
-    if(array[i] === array[i + 1]) {
-      count++
-      array.splice(i, 2)
-      return popDolls(array)
-    }
-  }
-  return count
-}
-
 function solution(board, moves) {
   let stack = []
-  //뽑은 인형 stack에 넣기
+  let count = 0
+
   moves.forEach((move) => {
-    for(let i = 0; i < 5; i++) {
-      if(board[i][move - 1] !== 0) {
-        stack.push(board[i][move - 1])
+    for(let i = 0; i < board.length; i++) {
+      let doll = board[i][move - 1] //뽑은 인형
+      if(doll !== 0) { //빈칸이 아니면
+        if(stack[stack.length - 1] === doll) { //stack의 맨 위에 담긴 인형과 같으면 count+2, 인형 pop
+          count += 2
+          stack.pop()
+        } else { //아니면 stack에 담기
+          stack.push(doll)
+        }
         board[i][move - 1] = 0
         break
       }
     }
   })
-
-  //count 1회 당 두 개의 인형이 사라지므로 2배로 리턴
-  return popDolls(stack) * 2
+  return count
 }
 
 board = [[0, 0, 0, 0, 0], [0, 0, 1, 0, 3], [0, 2, 5, 0, 1], [4, 2, 4, 4, 2], [3, 5, 1, 3, 1]]
