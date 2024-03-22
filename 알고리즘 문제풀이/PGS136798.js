5ec60964d782226ba06e26b3cf4a74f1489586ae
eugene9851 <eugenelee980713@gmail.com>
2024-03-22 10:05:29 +0900

    기사단원의 무기 리팩토링

--- 알고리즘 문제풀이/PGS136798.js
+++ 알고리즘 문제풀이/PGS136798.js
@@ -1,30 +1,28 @@
 //약수의 개수 구하는 함수
 function Yaksoo(number) {
-  let yaksooArray = [] //약수의 배열
+  let count = 0
 
-  for(let i = 1; i <= number; i++) {
-    if(number % i === 0) { //약수이면서
-      if(number / i > i) { //아직 구하지 않은 약수이면 둘 다 push
-        yaksooArray.push(i, number / i)
-      } else if(number / i === i) { //제곱수이면 중복되므로 하나만 push
-        yaksooArray.push(i)
-      } else break //모두 아니면 이미 약수들을 구한 것이므로 break
+  for(let i = 1; i * i <= number; i++) {
+    if(number % i === 0) {
+      if(number % i === 0) {
+        count += (number / i === i) ? 1 : 2
+      }
     }
   }
-  return yaksooArray.length
+  return count
 }
 
 function solution(number, limit, power) {
-  let weapons = [] //구매한 무기 공격력 배열
+  let weapons = 0
 
   //구매할 공격력이 limit보다 크면 power로 구매
   for(let i = 1; i <= number; i++) {
-    const weapon = Yaksoo(i) <= limit ? Yaksoo(i) : power
-    weapons.push(weapon)
+    const yaksooCount = Yaksoo(i)
+    const weapon = yaksooCount <= limit ? yaksooCount : power
+    weapons += weapon
   }
 
-  //배열의 모든 수 더하기
-  return weapons.reduce((acc, cur) => acc + cur, 0)
+  return weapons
 }
 
 number = 5
