56083c6c9feb05ee5f29a50142b1b3fcedab2ffb
eugene9851 <eugenelee980713@gmail.com>
2024-03-22 10:29:32 +0900

    [1차]캐시 리팩토링

--- 알고리즘 문제풀이/PGS17680.js
+++ 알고리즘 문제풀이/PGS17680.js
@@ -1,27 +1,24 @@
 function solution(cacheSize, cities) {
-  //대소문자 구분 안하므로 도시 전부 소문자로 바꾸기
-  const lowerCities = cities.map((city) => city.toLowerCase())
+  if(cacheSize === 0) return cities.length * 5 //캐시 크기가 0인 경우
 
-  let cache = [] //캐시
-  let time = 0 //실행시간
+  let time = 0
+  const cache = new Map()
 
-  lowerCities.forEach((city) => {
-    if(cache.length < cacheSize) { //초반에는 cache miss 처리
-      cache.push(city)
-      time += 5
-    } else if(cache.includes(city)) { //cache hit일 경우
-      if(cache[cacheSize - 1] === city) time++ //현재 city가 직전 city와 동일하면 time+1
-      else { //직전과 동일하지 않으면 위치 바꾸고 time+1
-        let temp = cache[cache.indexOf(city)]
-        cache[cache.indexOf(city)] = cache[cacheSize - 1]
-        cache[cacheSize - 1] = temp
-        time++
+  cities.forEach((city) => {
+    city = city.toLowerCase() //도시 이름을 소문자로 변환
+
+    if(cache.has(city)) {
+      time += 1 //cache hit
+      cache.delete(city)
+    } else {
+      if(cache.size === cacheSize) {
+        const oldest = cache.keys().next().value //가장 오래된 캐시 항목 찾기
+        cache.delete(oldest)
       }
-    } else { //cache miss일 경우
-      cache.shift()
-      cache.push(city)
-      time += 5
+      time += 5 //cache miss
     }
+
+    cache.set(city)
   })
   return time
 }
