e92fde72041a3d98402d7323ae1938c9ded7407c
eugene9851 <eugenelee980713@gmail.com>
2024-03-22 10:01:07 +0900

    실패율 리팩토링

--- 알고리즘 문제풀이/PGS42889.js
+++ 알고리즘 문제풀이/PGS42889.js
@@ -1,16 +1,24 @@
 function solution(stageNum, stages) {
-  let challengeNum = stages.length //challengeNum의 초기값은 stage의 길이
-  let failPercentage = []
+  const totalPlayers = stages.length //challengeNum의 초기값은 stage의 길이
+  const stageCounts = Array(stageNum + 1).fill(0)
+
+  //각 스테이지에 도전 중인 플레이어 수 계산
+  stages.forEach((stage) => {
+    if(stage <= stageNum) stageCounts[stage]++
+  })
+
+  let remainingPlayers = totalPlayers
+  const failRates = []
 
   for(let i = 1; i <= stageNum; i++) {
-    const failNum = stages.filter((stage) => stage === i).length //스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수
-    failPercentage.push({ id: i, fail: (failNum / challengeNum) }) //{id: 스테이지, fail: 실패율}
-    challengeNum -= failNum
+    const failRate = remainingPlayers > 0 ? stageCounts[i] / remainingPlayers : 0
+    failRates.push({ id: i, rate: failRate })
+    remainingPlayers -= stageCounts[i] //다음 스테이지의 도전자 수 갱신
   }
 
-  failPercentage.sort((a, b) => b.fail - a.fail) //실패율 내림차순
+  failRates.sort((a, b) => b.rate - a.rate) //실패율 내림차순
 
-  return failPercentage.map((obj) => obj.id) //정렬된 실패율에 해당하는 스테이지 리턴
+  return failRates.map((obj) => obj.id) //정렬된 실패율에 해당하는 스테이지 리턴
 }
 
 stageNum = 5
