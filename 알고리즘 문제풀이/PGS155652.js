3f26dd322e06d30a98e37290409d2527953f1774
eugene9851 <eugenelee980713@gmail.com>
2024-03-22 10:11:27 +0900

    둘만의 암호 리팩토링

--- 알고리즘 문제풀이/PGS155652.js
+++ 알고리즘 문제풀이/PGS155652.js
@@ -4,29 +4,26 @@
 
 function solution(string, skip, initialIndex) {
   //skip해야하는 아스키코드 배열
-  const skipCode = skip.split('').map((skipChar) => {
-    return skipChar.charCodeAt()
-  })
+  const skipCode = new Set(skip.split('').map(char => char.charCodeAt()))
 
-  const resultArray = string.split('').map((char) => {
-    const codeNum = char.charCodeAt() //각 알파벳의 아스키코드
+  let result = ""
 
+  for(const char of string) {
+    let codeNum = char.charCodeAt() //각 알파벳의 아스키코드
     let index = initialIndex
+    let nextCharCode = codeNum
 
-    //현재 알파벳과 index만큼 뒤의 알파벳 사이에 skipCode가 있으면 건너뛰기(= index++)
-    skipCode.forEach((code) => {
-      if(codeNum < code && code <= codeNum + index) index++
-    })
-
-    //resultCode가 122(z)보다 크면 26씩 빼기
-    let resultCode = codeNum + index
-    while(resultCode > 122) {
-      resultCode -= 26
+    for(let i = 1; i <= index; i++) {
+      nextCharCode++
+      if(nextCharCode > 122) nextCharCode = 97
+      while(skipCode.has(nextCharCode)) {
+        nextCharCode++
+        if(nextCharCode > 122) nextCharCode = 97
+      }
     }
-    return String.fromCharCode(resultCode)
-
-  })
-  return resultArray.join('')
+    result += String.fromCharCode(nextCharCode)
+  }
+  return result
 }
 
 s = "aukks"
