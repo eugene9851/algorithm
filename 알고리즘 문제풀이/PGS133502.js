36ef2c3dab081ec8253ff032a28d795054218a96
eugene9851 <eugenelee980713@gmail.com>
2024-03-25 12:47:27 +0900

    프로그래머스 햄버거 만들기

--- 알고리즘 문제풀이/PGS133502.js
+++ 알고리즘 문제풀이/PGS133502.js
@@ -1,20 +1,21 @@
 //+53
-const burger = [1, 2, 3, 1]
 let count = 0
 
 function solution(ingredient) {
-  let i = 0
-  while(ingredient.splice(i, 4) !== burger) {
-    i++
-  }
-  if(ingredient.splice(i, 4) === burger) {
-    count++
-    ingredient.splice(i, 4)
-    return solution(ingredient)
+  if(ingredient.length < 4) return count
+
+  let stringIngredient = ingredient.join('')
+
+  for(let i = 0; i < stringIngredient.length - 3; i++) {
+    if(stringIngredient.slice(i, i + 4) == "1231") {
+      count++
+      ingredient.splice(i, 4)
+      return solution(ingredient)
+    }
   }
   return count
 }
 
-// ingredient = [1, 3, 2, 1, 2, 1, 3, 1, 2]
-ingredient = [2, 1, 1, 2, 3, 1, 2, 3, 1]
+ingredient = [1, 3, 2, 1, 2, 1, 3, 1, 2]
+// ingredient = [2, 1, 1, 2, 3, 1, 2, 3, 1]
 console.log(solution(ingredient))
