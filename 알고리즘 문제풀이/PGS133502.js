69b51f3b5c3f9a428d740376004ac6497faedd90
eugene9851 <eugenelee980713@gmail.com>
2024-03-29 20:22:23 +0900

    햄버거 만들기 리팩토링

--- 알고리즘 문제풀이/PGS133502.js
+++ 알고리즘 문제풀이/PGS133502.js
@@ -1,21 +1,19 @@
-//+53
-let count = 0
-
 function solution(ingredient) {
-  if(ingredient.length < 4) return count
+  let count = 0
+  let i = 0
 
-  let stringIngredient = ingredient.join('')
-
-  for(let i = 0; i < stringIngredient.length - 3; i++) {
-    if(stringIngredient.slice(i, i + 4) == "1231") {
+  while(i <= ingredient.length - 4) {
+    if(ingredient[i] === 1 && ingredient[i + 1] === 2 && ingredient[i + 2] === 3 && ingredient[i + 3] === 1) {
+      ingredient.splice(i, 4) //해당 부분 제거
       count++
-      ingredient.splice(i, 4)
-      return solution(ingredient)
+      i = Math.max(i - 4, 0) //제거했다면 이전 위치에서 검색 continue
+    } else {
+      i++ //제거하지 못했다면 다음으로 continue
     }
   }
   return count
 }
 
-ingredient = [1, 3, 2, 1, 2, 1, 3, 1, 2]
-// ingredient = [2, 1, 1, 2, 3, 1, 2, 3, 1]
+// ingredient = [1, 3, 2, 1, 2, 1, 3, 1, 2]
+ingredient = [2, 1, 1, 2, 3, 1, 2, 3, 1]
 console.log(solution(ingredient))
