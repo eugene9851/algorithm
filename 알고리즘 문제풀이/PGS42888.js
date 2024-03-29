14df287507aee15a25863d77b4cd01663658b667
eugene9851 <eugenelee980713@gmail.com>
2024-03-29 21:11:05 +0900

    오픈채팅방 리팩토링

--- 알고리즘 문제풀이/PGS42888.js
+++ 알고리즘 문제풀이/PGS42888.js
@@ -1,17 +1,20 @@
 function solution(record) {
-  let info = {}
+  let userNicknames = {}
   //최종 닉네임 구하기
   record.forEach((description) => {
     const [inOrOut, id, name] = description.split(' ')
-    if(inOrOut === "Enter") info[id] = name
-    if(inOrOut === "Change") info[id] = name
+    if(inOrOut === "Enter" || inOrOut === "Change") userNicknames[id] = name
   })
 
-  return record.map((description) => {
-    const [inOrOut, id, name] = description.split(' ')
-    if(inOrOut === "Enter") return `${info[id]}님이 들어왔습니다.`
-    if(inOrOut === "Leave") return `${info[id]}님이 나갔습니다.`
-  }).filter((alarm) => alarm !== undefined)
+  return record.reduce((acc, description) => {
+    const [inOrOut, id] = description.split(' ')
+    if(inOrOut === "Enter") {
+      acc.push(`${userNicknames[id]}님이 들어왔습니다.`)
+    } else if(inOrOut === "Leave") {
+      acc.push(`${userNicknames[id]}님이 나갔습니다.`)
+    }
+    return acc
+  }, [])
 }
 
 record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"]
