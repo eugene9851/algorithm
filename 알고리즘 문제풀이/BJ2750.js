28d10c1368f67e4b200a33fcae7ccd6bd256bcab
eugene9851 <eugene9851@naver.com>
2025-06-17 18:54:58 +0900

    백준 수 정렬하기

--- 알고리즘 문제풀이/BJ2750.js
+++ 알고리즘 문제풀이/BJ2750.js
@@ -1,6 +1,19 @@
 let input = require('fs').readFileSync('../example.txt').toString().trim().split('\n');
 
 const N = Number(input[0]);
-const arr = input.slice(1).sort((a, b) => a - b)
+const arr = input.slice(1).map(Number);
 
-console.log(arr.join('\n'))
+function solution(N, arr) {
+    for (let i = 0; i < N - 1; i++) {
+        for (let j = i + 1; j < N; j++) {
+            if (arr[i] > arr[j]) {
+                let temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(solution(N, arr).join('\n'))
