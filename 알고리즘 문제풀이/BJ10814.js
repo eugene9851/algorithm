ef15f64f286152904ae7a0b3ae9cd12ebc0874d5
eugene9851 <eugene9851@naver.com>
2025-06-18 20:57:14 +0900

    백준 나이순 정렬

---
+++ 알고리즘 문제풀이/BJ10814.js
@@ -0,0 +1,45 @@
+let input = require('fs').readFileSync('../example.txt').toString().trim().split('\n');
+
+const n = Number(input[0])
+const arr = input.slice(1).map(v => {
+    const [age, name] = v.split(' ')
+    return [Number(age), name]
+})
+
+const merge = (left, right) => {
+    let pointer1 = 0
+    let pointer2 = 0
+    const result = []
+
+    while (pointer1 <= left.length && pointer2 <= right.length) {
+        if (pointer1 === left.length) {
+            result.push(...right.slice(pointer2))
+            break
+        } else if (pointer2 === right.length) {
+            result.push(...left.slice(pointer1))
+            break
+        }
+
+        if (left[pointer1][0] > right[pointer2][0]) {
+            result.push(right[pointer2])
+            pointer2++
+        } else {
+            result.push(left[pointer1])
+            pointer1++
+        }
+    }
+
+    return result
+}
+
+const mergeSort = (arr) => {
+    if (arr.length <= 1) return arr
+
+    const mid = Math.floor(arr.length / 2)
+    const left = arr.slice(0, mid)
+    const right = arr.slice(mid)
+
+    return merge(mergeSort(left), mergeSort(right))
+}
+
+console.log(mergeSort(arr).map(v => v.join(' ')).join('\n'))
