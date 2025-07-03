239d2ac02f55780f88b5695e41405782077305be
eugene9851 <eugene9851@naver.com>
2025-07-03 22:08:30 +0900

    백준 단지번호붙이기

--- 알고리즘 문제풀이/BJ2667.js
+++ 알고리즘 문제풀이/BJ2667.js
@@ -1,47 +1,46 @@
-//40min
-
 let input = require('fs').readFileSync('../example.txt').toString().trim().split('\n');
 
-const n = Number(input[0])
+const n = Number(input[0]);
+const graph = new Array(n).fill(null).map(() => []);
 
-let graph = new Array(n).fill(null).map(() => [])
-for(let i = 0; i < n; i++) {
-  graph[i].push(...input[i + 1].split('').map(Number))
+for (let i = 1; i <= n; i++) {
+  const row = input[i].split('').map(Number);
+  graph[i - 1].push(...row);
 }
 
-const visited = new Array(n).fill(null).map(() => new Array(n).fill(false))
-const dx = [0, 0, -1, 1]
-const dy = [-1, 1, 0, 0]
+const visited = new Array(n).fill(null).map(() => new Array(n).fill(false));
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
 
-let count_home = 0 //각 단지 안 집의 개수
-let count_total = 0 //전체 단지 수
-let answer = [] //count_home을 담은 배열
+function dfs(graph, visited, x, y) {
+  visited[x][y] = true;
+  count++;
 
-function dfs(x, y) {
-  if(graph[x][y] === 1 && visited[x][y] === false) {
-    visited[x][y] = true
-    count_home++
+  for (let i = 0; i < 4; i++) {
+    const nx = x + dx[i];
+    const ny = y + dy[i];
+    if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visited[nx][ny] && graph[nx][ny] === 1) {
+      dfs(graph, visited, nx, ny);
+    }
+  }
+}
 
-    for(let i = 0; i < 4; i++) {
-      let nx = x + dx[i]
-      let ny = y + dy[i]
-      if(nx >= 0 && nx < n && ny >= 0 && ny < n) {
-        dfs(nx, ny)
+let count = 0;
+const result = [];
+
+function solution(graph, visited, n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (!visited[i][j] && graph[i][j] === 1) {
+        dfs(graph, visited, i, j);
+        result.push(count);
+        count = 0;
       }
     }
   }
+  return result.sort((a, b) => a - b);
 }
 
-for(let i = 0; i < n; i++) {
-  for(let j = 0; j < n; j++) {
-    if(graph[i][j] === 1 && visited[i][j] === false) {
-      dfs(i, j)
-      count_total++
-      answer.push(count_home)
-      count_home = 0
-    }
-  }
-}
-
-console.log(count_total)
-console.log(answer.sort((a, b) => a - b).join('\n'))
+solution(graph, visited, n);
+console.log(result.length);
+console.log(result.join('\n'));
