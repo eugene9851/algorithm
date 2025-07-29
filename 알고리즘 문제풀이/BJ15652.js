c310da72c366d9dc4800f468a4a7067384411cbe
eugene9851 <eugene9851@naver.com>
2025-07-29 21:27:51 +0900

    백준 N과 M(4)

---
+++ 알고리즘 문제풀이/BJ15652.js
@@ -0,0 +1,19 @@
+let input = require('fs').readFileSync('../example.txt').toString().trim();
+
+const [n, m] = input.split(' ').map(Number);
+
+function backtracking(n, m, arr) {
+    if (arr.length === m) {
+        console.log(arr.join(' '));
+        return;
+    }
+
+    for (let i = 1; i <= n; i++) {
+        if (arr[arr.length - 1] > i) continue;
+        arr.push(i);
+        backtracking(n, m, arr);
+        arr.pop();
+    }
+}
+
+backtracking(n, m, []);
