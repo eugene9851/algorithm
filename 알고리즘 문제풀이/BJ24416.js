4ad0c4f6b98809f0d5545fe8c07452f16cc89130
eugene9851 <eugene9851@naver.com>
2025-08-04 20:46:19 +0900

    백준 알고리즘 수업-피보나치 수 1

---
+++ 알고리즘 문제풀이/BJ24416.js
@@ -0,0 +1,26 @@
+const n = require('fs').readFileSync('../example.txt')
+
+let fibCount = 1;
+function fib(n) {
+    if (n === 1 || n === 2) return 1;
+    fibCount++;
+    return fib(n - 1) + fib(n - 2);
+}
+
+
+function fibonacci(n) {
+    let count = 0;
+    const f = [];
+    f[1] = 1
+    f[2] = 1
+
+    for (let i = 3; i <= n; i++) {
+        f[i] = f[i - 1] + f[i - 2]
+        count++;
+    }
+    return count;
+}
+
+fib(n);
+
+console.log(fib(n), fibonacci(n));
